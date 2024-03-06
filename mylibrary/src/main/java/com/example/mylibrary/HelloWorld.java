d13678822b95085298d716c408003a3219fe0b19
Pheav Cheavkin <Cheavkin.Pheav@amkcambodia.com>
2024-03-06 16:16:43 +0700

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+public class HelloWorld {
+    public static void main(String[] args){
+
+    }
+    public static String sayHello(String smg){
+        return  "From MyLibs: "+smg;
+    }
+}

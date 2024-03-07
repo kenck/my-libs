9ef23c326966799a42747d6a75e2dcc8b9c157e4
Pheav Cheavkin <Cheavkin.Pheav@amkcambodia.com>
2024-03-07 08:15:45 +0700

    first commit

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -33,11 +33,21 @@ android {
     }
 }
 
-dependencies {
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
 
+dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
